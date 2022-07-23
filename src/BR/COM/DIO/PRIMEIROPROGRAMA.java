b162666d3e9858ba5d7bc10d15015ee7b97ffffd
Paloma <paloma_lyssamg@outlook.com>
2022-07-23 13:59:31 -0300

    first commit

---
+++ src/BR/COM/DIO/PRIMEIROPROGRAMA.java
@@ -0,0 +1,27 @@
+package BR.COM.DIO;
+
+import BR.COM.DIO.MODEL.GATO;
+
+public class PRIMEIROPROGRAMA {
+
+	public static void main(String[] args) {
+		
+		GATO GATO = new GATO();
+		Livros livros = new Livros();
+		
+		System.out.println(GATO);
+		System.out.println(livros);
+		
+		/*int a = 2;
+		int b = 3;
+		System.out.println("Hello World!" + (a+b));*/	
+	}
+}
+	
+
+class Livros {
+	private String nome;
+	private String npag; 
+		
+}	
+	
